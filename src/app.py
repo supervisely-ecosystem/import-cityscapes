a14f72d5559f83aa0c9c680a7b593f153dd8de4e
Alex Emelyanov <alex84_84_84@mail.ru>
2021-04-18 10:57:45 +0300

    test

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ from supervisely_lib.video_annotation.video_tag_collection import VideoTagCollec
 my_app = sly.AppService()
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
-INPUT_DIR = os.environ['context.Folder']
+INPUT_DIR = str(os.environ['context.Folder'])
 #INPUT_DIR = os.environ.get("modal.state.slyFolder")
 
 project_name = 'cityscapes'
